8391a38f338b0c579f91720d3ff23102f2468e81
Vladislav Rishe <vlados.v10.0@gmail.com>
2018-09-25 00:30:18 +0300

    Re-factor: DiscDialer's implemntation.
    DiscDialer's implementation unburdened by moving the Rotor class into a separate file.

--- disc_dialer_lib/src/main/java/com/example/disc_dialer_lib/DiscDialer.java
+++ disc_dialer_lib/src/main/java/com/example/disc_dialer_lib/DiscDialer.java
@@ -3,10 +3,9 @@ package com.example.disc_dialer_lib;
 import android.annotation.SuppressLint;
 import android.content.Context;
 import android.graphics.Canvas;
-import android.graphics.PointF;
 import android.graphics.RectF;
 import android.os.Build;
-import android.os.SystemClock;
+import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.annotation.RequiresApi;
 import android.support.v4.content.ContextCompat;
@@ -16,9 +15,7 @@ import android.view.View;
 import java.util.ArrayList;
 import java.util.Collection;
 
-public final class DiscDialer extends View {
-
-  private static final String TAG = DiscDialer.class.getName();
+public final class DiscDialer extends View implements Rotor.PulseInputReceiver {
 
   public interface InputListener {
     void onDigitInput(DiscDialer dialer, int digit);
@@ -34,21 +31,6 @@ public final class DiscDialer extends View {
     void setClipRect(RectF clipRect);
   }
 
-  private static final double RADIAN = Math.PI / 180;
-  private static final double RADIAN_INV = 180 / Math.PI;
-
-  private static class DialerConfig {
-    public double discAngularVelocity = 40./1000; // degrees per millisecond
-    public double knobAzimuth = 25 * RADIAN; // radians
-    public float outerDeadZoneCoeff = 1.f;
-    public float innerDeadZoneCoeff = .58103f;
-    public float innerDeadZoneGripMult = .16f;
-    public float cockAngleThreshold = 52f; // degrees;
-    public float digitSegmentArc = 28f; // degrees
-  }
-
-  private DialerConfig _config;
-  private Renderer _renderer;
 
   public DiscDialer(Context context) {
     super(context);
@@ -72,7 +54,9 @@ public final class DiscDialer extends View {
     init(context, attrs, defStyleAttr, defStyleRes);
   }
 
+
   private final Collection<InputListener> _listeners = new ArrayList<>();
+
   public void addListener(InputListener listener) {
     _listeners.add(listener);
   }
@@ -81,46 +65,31 @@ public final class DiscDialer extends View {
     _listeners.remove(listener);
   }
 
+  public void receivePulseInput(int digit) {
+    for (InputListener l: _listeners) {
+      l.onDigitInput(this, digit);
+    }
+  }
+
+
+  private Renderer _renderer;
+
+  public void setRenderer(@NonNull Renderer renderer) {
+    _renderer = renderer;
+    invalidate();
+  }
+
+  private final Rotor _rotor = new Rotor(this, this);
+
   private void init(Context context, @Nullable AttributeSet attrs, int defStyleAttr,
       int defStyleRes) {
-    _config = new DialerConfig();
     _renderer = new DrawableRenderer(
         ContextCompat.getDrawable(context, R.drawable.disc_dialer_bg),
         ContextCompat.getDrawable(context, R.drawable.disc_dialer_disc),
         ContextCompat.getDrawable(context, R.drawable.disc_dialer_fg));
   }
 
-  private static double arc(PointF center, float ax, float ay, float bx, float by) {
-    ax -= center.x;
-    ay -= center.y;
-    bx -= center.x;
-    by -= center.y;
-
-    final double s = (ax * by - ay * bx)
-        / Math.sqrt((ax * ax + ay * ay) * (bx * bx + by * by));
-
-    return Math.asin(s);
-  }
-
-  private static double azimuth(PointF center, float x, float y) {
-    double a = Math.atan2(y - center.y, x - center.x);
-    return a < 0 ? 2 * Math.PI + a : a;
-  }
-
-  private static long getAnimationTime() {
-    return SystemClock.elapsedRealtime();
-  }
-
-  private float getSquareDistance(PointF p, float x, float y) {
-    x -= p.x;
-    y -= p.y;
-
-    return x*x+y*y;
-  }
-
-
   private final RectF _clipRect = new RectF();
-  private final Rotor _rotor = new Rotor();
 
   @Override protected void onDraw(Canvas canvas) {
     super.onDraw(canvas);
@@ -159,129 +128,5 @@ public final class DiscDialer extends View {
   @Override @SuppressLint("ClickableViewAccessibility")
   public boolean onTouchEvent(MotionEvent event) {
     return _rotor.onTouchEvent(event);
-  }
-
-  private final class Rotor {
-    float angle;
-
-    private int _pulsesCount;
-
-    void dispatchPulsesCount() {
-      final int digit = _pulsesCount%10;
-      for (InputListener l: _listeners) {
-        l.onDigitInput(DiscDialer.this, digit);
-      }
-    }
-
-    private boolean _debounce;
-    private long _t0;
-
-    void doAnimationTick() {
-      if (_debounce) {
-        long dt = getAnimationTime() - _t0;
-        angle -= dt * _config.discAngularVelocity;
-
-        if (angle < 0) {
-          angle = 0;
-          _debounce = false;
-        }
-        if (0 < _pulsesCount
-            && angle < _config.digitSegmentArc) {
-          dispatchPulsesCount();
-          _pulsesCount = 0;
-        }
-        postInvalidate();
-      }
-    }
-
-
-    private final PointF _pivot = new PointF();
-    private final PointF _touch = new PointF();
-
-    private double _phi0, _phi1;
-    private float _radiusOuter, _radiusInner;
-
-    void setPivot(float cx, float cy, float radius) {
-      _pivot.set(cx, cy);
-      _radiusOuter = radius*_config.outerDeadZoneCoeff;
-      _radiusOuter *= _radiusOuter;
-      _radiusInner = radius*_config.innerDeadZoneCoeff;
-      _radiusInner *= _radiusInner;
-    }
-
-    private float _gripMult;
-    private float _maxAngle;
-
-    float getMaxAngle(double phi0) {
-      phi0 = RADIAN_INV * (2*Math.PI - phi0);
-      if (phi0 > _config.cockAngleThreshold) {
-        float theta = _config.cockAngleThreshold;
-        for (int i = 1; i <= 10; ++i) {
-          if (phi0 < theta + _config.digitSegmentArc) return theta;
-
-          theta += _config.digitSegmentArc;
-        }
-      }
-      return 360;
-    }
-
-    boolean onTouchEvent(MotionEvent event) {
-      final int action = event.getAction();
-      switch (action) {
-        case MotionEvent.ACTION_DOWN:
-          _debounce = false;
-          _touch.set(event.getX(), event.getY());
-          _phi0 = azimuth(_pivot, _touch.x, _touch.y) - _config.knobAzimuth;
-          _phi1 = _phi0;
-          _maxAngle = getMaxAngle(_phi0);
-          float r = getSquareDistance(_pivot, _touch.x, _touch.y);
-          _gripMult = r < _radiusInner ?
-              _config.innerDeadZoneGripMult * r / _radiusInner : 1.f;
-          return _clipRect.contains(_touch.x, _touch.y)
-              && _radiusOuter > r && _gripMult > 0;
-
-        case MotionEvent.ACTION_MOVE:
-          double alpha = arc(_pivot, _touch.x, _touch.y, event.getX(), event.getY());
-          double phi = _phi0 + alpha * _gripMult;
-
-          _touch.set(event.getX(), event.getY());
-          _phi0 = phi;
-
-          if (phi < 0) phi = 0;
-          if (phi > 2 * Math.PI) phi = 2 * Math.PI;
-          alpha = phi - _phi1;
-          _phi1 = phi;
-
-          if (alpha != 0) {
-            float angle_old = angle;
-            angle += RADIAN_INV * alpha;
-            if (angle < 0) angle = 0;
-            if (angle > _maxAngle) angle = _maxAngle;
-
-            if (alpha > 0) {
-              _pulsesCount = Math.round((angle - _config.cockAngleThreshold) / _config.digitSegmentArc);
-              if (_pulsesCount > 10) _pulsesCount = 10;
-            }
-            if (angle != angle_old)
-              invalidate();
-          }
-          if (0 < _pulsesCount
-              && angle < _config.digitSegmentArc) {
-            dispatchPulsesCount();
-            _pulsesCount = 0;
-          }
-          break;
-
-        case MotionEvent.ACTION_CANCEL:
-        case MotionEvent.ACTION_UP:
-          _debounce = angle > 0;
-          if (_debounce) {
-            _t0 = getAnimationTime();
-            postInvalidate();
-          }
-          break;
-      }
-      return true;
-    }
   }
 }
---
+++ disc_dialer_lib/src/main/java/com/example/disc_dialer_lib/Rotor.java
@@ -0,0 +1,177 @@
+package com.example.disc_dialer_lib;
+
+import android.graphics.PointF;
+import android.os.SystemClock;
+import android.support.annotation.NonNull;
+import android.view.MotionEvent;
+import android.view.View;
+
+final class Rotor {
+
+  private static final int MAX_PULSES = 10;
+
+  interface PulseInputReceiver {
+    void receivePulseInput(int pulseCount);
+  }
+
+  private final View _view;
+  private PulseInputReceiver _dispatcher;
+
+  Rotor(@NonNull View view, @NonNull PulseInputReceiver dispatcher) {
+    _view = view;
+    _dispatcher = dispatcher;
+  }
+
+  private static double arc(PointF center, float ax, float ay, float bx, float by) {
+    ax -= center.x;
+    ay -= center.y;
+    bx -= center.x;
+    by -= center.y;
+
+    final double s = (ax * by - ay * bx) / Math.sqrt((ax * ax + ay * ay) * (bx * bx + by * by));
+
+    return Math.asin(s);
+  }
+
+  private static double azimuth(PointF center, float x, float y) {
+    double a = Math.atan2(y - center.y, x - center.x);
+    return a < 0 ? 2 * Math.PI + a : a;
+  }
+
+  private static long getAnimationTime() {
+    return SystemClock.elapsedRealtime();
+  }
+
+  private static float getSquareDistance(PointF p, float x, float y) {
+    x -= p.x;
+    y -= p.y;
+
+    return x * x + y * y;
+  }
+
+  private static final double RADIAN = Math.PI / 180;
+  private static final double RADIAN_INV = 180 / Math.PI;
+
+  private static class DialerConfig {
+    double discAngularVelocity = 40. / 1000; // degrees per millisecond
+    double knobAzimuth = 25 * RADIAN; // radians
+    float outerDeadZoneCoeff = 1.f;
+    float innerDeadZoneCoeff = .58103f;
+    float innerDeadZoneGripMult = .16f;
+    float cockAngleThreshold = 52f; // degrees;
+    float digitSegmentArc = 28f; // degrees
+  }
+
+  private DialerConfig _config = new DialerConfig();
+
+  float angle;
+
+  private boolean _debounce;
+  private long _t0;
+
+  public void doAnimationTick() {
+    if (_debounce) {
+      long dt = getAnimationTime() - _t0;
+      angle -= dt * _config.discAngularVelocity;
+
+      if (angle < 0) {
+        angle = 0;
+        _debounce = false;
+      }
+      discardPulsesCount();
+      _view.postInvalidate();
+    }
+  }
+
+  private final PointF _pivot = new PointF();
+  private final PointF _touch = new PointF();
+
+  private double _phi0, _phi1;
+  private float _gripMult;
+  private float _maxAngle;
+  private float _radiusOuter, _radiusInner;
+  private int _pulsesCount;
+
+  public boolean onTouchEvent(MotionEvent event) {
+    final int action = event.getAction();
+    switch (action) {
+      case MotionEvent.ACTION_DOWN:
+        _debounce = false;
+        _touch.set(event.getX(), event.getY());
+        _phi0 = azimuth(_pivot, _touch.x, _touch.y) - _config.knobAzimuth;
+        _phi1 = _phi0;
+        _maxAngle = getMaxAngle(_phi0);
+        float r = getSquareDistance(_pivot, _touch.x, _touch.y);
+        _gripMult = r < _radiusInner ? _config.innerDeadZoneGripMult * r / _radiusInner : 1.f;
+        return _radiusOuter >= r && _gripMult > 0;
+
+      case MotionEvent.ACTION_MOVE:
+        double alpha = arc(_pivot, _touch.x, _touch.y, event.getX(), event.getY());
+        double phi = _phi0 + alpha * _gripMult;
+
+        _touch.set(event.getX(), event.getY());
+        _phi0 = phi;
+
+        if (phi < 0) phi = 0;
+        if (phi > 2 * Math.PI) phi = 2 * Math.PI;
+        alpha = phi - _phi1;
+        _phi1 = phi;
+
+        if (alpha != 0) {
+          float angle_old = angle;
+
+          angle += RADIAN_INV * alpha;
+          if (angle < 0) angle = 0;
+          if (angle > _maxAngle) angle = _maxAngle;
+
+          if (alpha > 0) _pulsesCount = Math.min(getPulsesCountEstimate(), MAX_PULSES);
+          if (angle != angle_old) _view.invalidate();
+        }
+        discardPulsesCount();
+        break;
+
+      case MotionEvent.ACTION_CANCEL:
+      case MotionEvent.ACTION_UP:
+        _debounce = angle > 0;
+        if (_debounce) {
+          _t0 = getAnimationTime();
+          _view.postInvalidate();
+        }
+        break;
+    }
+    return true;
+  }
+
+  public void setPivot(float cx, float cy, float radius) {
+    _pivot.set(cx, cy);
+    _radiusOuter = radius * _config.outerDeadZoneCoeff;
+    _radiusOuter *= _radiusOuter;
+    _radiusInner = radius * _config.innerDeadZoneCoeff;
+    _radiusInner *= _radiusInner;
+  }
+
+  private void discardPulsesCount() {
+    if (_pulsesCount > 0 && angle < _config.digitSegmentArc) {
+      _dispatcher.receivePulseInput(_pulsesCount % MAX_PULSES);
+      _pulsesCount = 0;
+    }
+  }
+
+  private int getPulsesCountEstimate() {
+    return Math.round((angle - _config.cockAngleThreshold) / _config.digitSegmentArc);
+  }
+
+  private float getMaxAngle(double phi0) {
+    phi0 = RADIAN_INV * (2 * Math.PI - phi0);
+    if (phi0 <= _config.cockAngleThreshold)
+      return 360;
+
+    float theta = _config.cockAngleThreshold;
+    for (int i = 1; i <= MAX_PULSES; ++i) {
+      if (phi0 < theta + _config.digitSegmentArc) return theta;
+
+      theta += _config.digitSegmentArc;
+    }
+    return theta;
+  }
+}
